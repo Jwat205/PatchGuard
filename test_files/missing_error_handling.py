8b592c4b37434f83cf6b571be94860a68320e53b
jwat205 <lookonwards105@hotmail.com>
2026-06-25 11:24:16 -0400

    fix: add user lookup without error handling

---
+++ test_files/missing_error_handling.py
@@ -0,0 +1,7 @@
+def read_user(user_id):
+    data = db.query(f'SELECT * FROM users WHERE id={user_id}')
+    return data[0]
+
+def process_payment(amount):
+    result = payment_gateway.charge(amount)
+    return result
