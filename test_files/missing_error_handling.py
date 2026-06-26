44ae0570902a4d3b4a939c63be4bde2556a0659c
Jesse Watson <lookonwards105@gmail.com>
2026-06-25 22:42:52 -0400

    Delete test_files/missing_error_handling.py

--- test_files/missing_error_handling.py
+++
@@ -1,8 +0,0 @@
-def read_user(user_id):
-    data = db.query(f'SELECT * FROM users WHERE id={user_id}')
-    return data[0]
-
-def process_payment(amount):
-    #test(retry)
-    result = payment_gateway.charge(amount)
-    return result
