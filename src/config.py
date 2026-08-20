378f078549cbf7dfc5b0c24f9824e3860feb17e7
Jwat205 <watson.jlee@ufl.edu>
2026-08-20 00:20:27 -0400

    Revert "Resolve merge conflicts"
    This reverts commit 137fc9516de18069adde2e31f1475ed1bb60bea0.

--- src/config.py
+++ src/config.py
@@ -46,12 +46,6 @@ class Settings(BaseSettings):
     kafka_consumer_group: str = "patchguard-consumers"
 
     # API
-<<<<<<< Updated upstream
-=======
-    import os
-
-# Inside your config class or file:
->>>>>>> Stashed changes
     host: str = os.getenv("HOST", "127.0.0.1")
     port: int = int(os.getenv("PORT", 8000))
 
